177ff69ff58be20e7e6f77f80fca7fff4eb82b7c
NikolaSmiljanic <nikola94nwa@gmail.com>
2017-06-16 23:20:52 +0200

    zapoceto siforvanje na delu national bank.

---
+++ National_bank_xml/src/main/java/encryption/IssuerData.java
@@ -0,0 +1,33 @@
+package encryption;
+
+import java.security.PrivateKey;
+
+import org.bouncycastle.asn1.x500.X500Name;
+
+public class IssuerData {
+
+	private PrivateKey privateKey;
+	private X500Name x500name;
+	
+	
+	public IssuerData(PrivateKey privateKey, X500Name x500name) {
+		super();
+		this.privateKey = privateKey;
+		this.x500name = x500name;
+	}
+	public PrivateKey getPrivateKey() {
+		return privateKey;
+	}
+	public void setPrivateKey(PrivateKey privateKey) {
+		this.privateKey = privateKey;
+	}
+	public X500Name getX500name() {
+		return x500name;
+	}
+	public void setX500name(X500Name x500name) {
+		this.x500name = x500name;
+	}
+	
+
+	
+}
